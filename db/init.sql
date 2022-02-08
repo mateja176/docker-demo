54bc8bea4a854a029f9656e16a2c8f004c4f8c3f
Mateja <mateja176@gmail.com>
2022-02-09 00:29:43 +0100

    adds db/init.sql

---
+++ db/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE hotspots
+(
+  address VARCHAR(55) PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO hotspots
+  (address, name)
+VALUES
+  ('11fjCGUMqjd2vnstQhYmLiuB8P66Tz3cM7MqdWioT9j9qdip5Vr', 'tall-cedar-mandrill');
