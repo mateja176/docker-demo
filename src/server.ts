e5a55670060a1468a5f7db29275c1388c048109b
Mateja <mateja176@gmail.com>
2022-02-09 02:28:20 +0100

    adds server to docker-compose

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
 import express from 'express';
+import path from 'path';
 import pg from 'pg';
+
+dotenv.config({
+  path: path.resolve(
+    process.cwd(),
+    `.env${process.env.NODE_ENV ? `.${process.env.NODE_ENV}` : ''}`,
+  ),
+});
 
 const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 if (Number.isNaN(port)) {
   throw new Error('"PORT" must be an integer');
 }
 
+const dbHost = process.env.POSTGRES_HOST;
+if (!dbHost) {
+  throw new Error('"POSTGRES_HOST" is required');
+}
+const dbPort = process.env.POSTGRES_PORT
+  ? Number(process.env.POSTGRES_PORT)
+  : 5432;
+if (Number.isNaN(dbPort)) {
+  throw new Error('"POSTGRES_PORT" must be an integer');
+}
 const dbUser = process.env.POSTGRES_USER;
 if (!dbUser) {
   throw new Error('"POSTGRES_USER" is required');
@@ -14,12 +32,6 @@ if (!dbUser) {
 const dbPassword = process.env.POSTGRES_PASSWORD;
 if (!dbPassword) {
   throw new Error('"POSTGRES_PASSWORD" is required');
-}
-const dbPort = process.env.POSTGRES_PORT
-  ? Number(process.env.POSTGRES_PORT)
-  : 5432;
-if (Number.isNaN(dbPort)) {
-  throw new Error('"POSTGRES_PORT" must be an integer');
 }
 const db = process.env.POSTGRES_DB;
 if (!db) {
@@ -32,9 +44,10 @@ app.use(express.json());
 
 (async () => {
   const client = new pg.Client({
+    host: dbHost,
+    port: dbPort,
     user: dbUser,
     password: dbPassword,
-    port: dbPort,
     database: db,
   });
 
